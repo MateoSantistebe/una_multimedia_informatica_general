53837688a93a9c2bfd2f04669446b00503dd818d
Mateo <mateosantistebe@hotmail.com>
2025-09-18 04:27:17 +0200

    Delete afonso_buffarini_chiapetti_frias_santistebe_preentrega_tp2 directory

--- afonso_buffarini_chiapetti_frias_santistebe_preentrega_tp2/funciones.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// funciones
-void mostrarMenuPrincipal();
-void mostrarOpciones();
-void mostrarCreditos();
-void limpiarYDibujarMarco();
-void esperarEnter(int y); // Ahora le pasamos la coordenada 'y' donde imprimir
